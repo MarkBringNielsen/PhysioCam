28c474ab6d5d7b5fa4a4fe94da6b490fabebe4ea
MarkBringNielsen <markbringnielsen@gmail.com>
2021-05-07 10:00:10 +0200

    Started on listview

---
+++ PhysioCam/PhysioCam/Data/Exercise.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace PhysioCam.Data
+{
+    class Exercise
+    {
+
+        public string name { get; set; }
+        public List<Image> Images { get; set; }
+
+
+    }
+}
