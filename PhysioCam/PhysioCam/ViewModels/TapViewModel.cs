db43ba9895cd7c614c32bb8ed34ef882ab4e17e1
MarkBringNielsen <markbringnielsen@gmail.com>
2021-05-27 12:18:11 +0200

    updated logo on main and imagebutton with text

---
+++ PhysioCam/PhysioCam/ViewModels/TapViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Diagnostics;
+using PhysioCam.ExercisePages;
+using System.Threading.Tasks;
+
+namespace PhysioCam.ViewModels
+{
+    class TapViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private readonly ICommand _tapCommand;
+        private readonly INavigation _navigation;
+        private TrainingProgramViewModel _viewModel;
+
+        public TapViewModel(INavigation navigation)
+        {
+            _viewModel = DependencyService.Get<TrainingProgramViewModel>();
+            _tapCommand = new Command (async () => await OnTapped());
+            _navigation = navigation;
+        }
+
+        public ICommand TapCommand
+        {
+            get { return _tapCommand; }
+        }
+
+        async Task OnTapped()
+        {
+            _viewModel.NewTrainingProgram();
+            ExercisePage exercisePage = new ExercisePage();
+            await _navigation.PushAsync(exercisePage);
+            _navigation.InsertPageBefore(new TrainingProgramPage(), exercisePage);
+        }
+    }
+}
---
+++ PhysioCam/PhysioCam/Views/ImageButtonWithText.xaml.cs
@@ -0,0 +1,25 @@
+﻿using PhysioCam.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PhysioCam.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ImageButtonWithText : ContentView
+    {
+        public ImageButtonWithText()
+        {
+            InitializeComponent();
+
+            BindingContext = new TapViewModel(Navigation);
+        }
+
+       
+    }
+}
